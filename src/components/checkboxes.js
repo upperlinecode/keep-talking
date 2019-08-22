cf9b8c706def8bef09e73519033506bbf31b8091
jolson615 <jolson615@gmail.com>
2019-08-22 10:19:21 -0400

    fixes merge conflict for checkboxes

--- src/components/checkboxes.js
+++ src/components/checkboxes.js
@@ -46,25 +46,14 @@ const Checkboxes = () => {
       <div className={"Checkboxes " + (component.winning(arrayOfChecks) ? "winning" : "losing")}>
         <h1 className="directions">Check only the correct boxes</h1>
         <div className="hint">
-<<<<<<< HEAD
-          <h3>Target Sum: 7</h3>
+          <h3>Target Sum: 3</h3>
           <h3>Your Sum: {component.countTrue(arrayOfChecks)}</h3>
         </div>
         <div className="checkGrid">
-          <div className={"checkbox " + arrayOfChecks[0]} onClick={()=>component.handleClickForBox(0)}>{arrayOfChecks[0]}</div>
+          <div className={"checkbox " + arrayOfChecks[0]} onClick={()=>component.handleClickForBox(0)}/>
           <div className={"checkbox " + arrayOfChecks[1]} onClick={()=>component.handleClickForBox(1)}/>
-          <div className={"checkbox " + arrayOfChecks[2]} onClick={()=>component.handleClickForBox(2)}/>
-          <div className={"checkbox " + arrayOfChecks[3]} onClick={()=>component.handleClickForBox(3)}/>
-=======
-          <h3>Target Sum: 3</h3>
-          <h3>Your Sum: {countTrue(arrayOfChecks)}</h3>
-        </div>
-        <div className="checkGrid">
-          <div className={"checkbox " + arrayOfChecks[0]} onClick={()=>handleClickForBox(0)}/>
-          <div className={"checkbox " + arrayOfChecks[1]} onClick={()=>handleClickForBox(1)}/>
           <div className={"checkbox "} />
           <div className={"checkbox "} />
->>>>>>> 555b999189ee686d8612f74396bdb879c08e3d3a
         </div>
       </div>
     )
